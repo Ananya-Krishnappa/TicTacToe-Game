04d6f9d25923b01d4aab52b6b4d9de4ad683a7e9
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-06-04 20:26:35 +0530

    Added:user input is added

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,9 +1,14 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 	public static void main(String[] args) {
 		char[] board = new char[10];
+		boolean gameEnded = false;
 		drawBoard(board);
+		String player1 = "";
+		player1 = selectLetter(player1);
 	}
 
 	/**
@@ -16,5 +21,29 @@ public class TicTacToeGame {
 		System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
 		System.out.println("--|---|--");
 		System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+	}
+
+	/**
+	 * @param player1
+	 * @return
+	 */
+	private static String selectLetter(String player1) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Let's play Tic Tac Toe!");
+		System.out.print("Select a letter X or O ");
+		String p1 = in.nextLine();
+		if (p1.equalsIgnoreCase("X")) {
+			System.out.println("Player1 selects X");
+			System.out.println("Player2 to play with O");
+			player1 = "X";
+		} else if (p1.equalsIgnoreCase("0")) {
+			System.out.println("Player1 selects O");
+			System.out.println("Player2 to play with X");
+			player1 = "O";
+		} else {
+			System.out.println("Please select a valid value");
+			selectLetter(player1);
+		}
+		return player1;
 	}
 }
