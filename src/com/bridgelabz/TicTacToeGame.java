809586a58fec29857f2ea93b733a724723edbfb9
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-06-04 19:51:30 +0530

    Added:board created

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -2,6 +2,19 @@ package com.bridgelabz;
 
 public class TicTacToeGame {
 	public static void main(String[] args) {
-		System.out.println("Welcome to tic tac toe game!");
+		char[] board = new char[10];
+		drawBoard(board);
+	}
+
+	/**
+	 * @param board
+	 */
+	private static void drawBoard(char[] board) {
+
+		System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+		System.out.println("--|---|--");
+		System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+		System.out.println("--|---|--");
+		System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
 	}
 }
