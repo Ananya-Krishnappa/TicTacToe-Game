5bbe9c921b1cc365c4428847f8e2664c3034361f
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-06-05 18:50:04 +0530

    Added:suggestion to the player

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -3,25 +3,30 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	public static void main(String[] args) {
-		char[] board = new char[10];
-		boolean gameEnded = false;
-		boolean player1Turn = true;
-		drawBoard(board);
-		String player1Letter = "";
-		player1Letter = selectLetter(player1Letter);
-		player1Turn = toss(player1Turn);
-		drawBoard(board);
-		System.out.println("Select an index between 1 to 9");
-		makeAMove(player1Turn, player1Letter, board);
+	private char[] board = new char[10];
+	private boolean player1Turn = true;
+	private String player1Letter = "";
+	private String gameStatusMessage = "";
+	Scanner scanner = new Scanner(System.in);
 
+	public static void main(String[] args) {
+		TicTacToeGame ticTacToeGame = new TicTacToeGame();
+		Scanner scanner = new Scanner(System.in);
+		ticTacToeGame.createBoard(ticTacToeGame);
+		ticTacToeGame.toss(ticTacToeGame);
+		ticTacToeGame.selectLetter(ticTacToeGame);
+		ticTacToeGame.createBoard(ticTacToeGame);
+		System.out.println("Select an index between 1 to 9");
+		ticTacToeGame.makeAMove(ticTacToeGame);
 	}
 
 	/**
-	 * @param board
+	 * This method is used to create a board
+	 * 
+	 * @param ticTacToeGame
 	 */
-	private static void drawBoard(char[] board) {
-
+	private void createBoard(TicTacToeGame ticTacToeGame) {
+		char[] board = ticTacToeGame.board;
 		System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
 		System.out.println("--|---|--");
 		System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
@@ -30,89 +35,172 @@ public class TicTacToeGame {
 	}
 
 	/**
-	 * @param player1
-	 * @return
+	 * This method is checking whether to enter X or 0
+	 * 
+	 * @param ticTacToeGame
 	 */
-	private static String selectLetter(String player1Letter) {
-		Scanner in = new Scanner(System.in);
-		System.out.println("Let's play Tic Tac Toe!");
-		System.out.print("Player1 to select a letter X or 0 ");
-		String p1 = in.nextLine();
+	private void selectLetter(TicTacToeGame ticTacToeGame) {
+		String player = ticTacToeGame.player1Turn ? "Player1" : "Player2";
+		System.out.print(player + " to select a letter X or 0 ");
+		String p1 = scanner.nextLine();
 		if (p1.equalsIgnoreCase("X")) {
-			System.out.println("Player1 selects X");
-			System.out.println("Player2 to play with 0");
-			player1Letter = "X";
+			player1Letter = ticTacToeGame.player1Turn ? p1.toUpperCase() : "0";
 		} else if (p1.equalsIgnoreCase("0")) {
-			System.out.println("Player1 selects 0");
-			System.out.println("Player2 to play with X");
-			player1Letter = "0";
+			player1Letter = ticTacToeGame.player1Turn ? p1.toUpperCase() : "X";
 		} else {
 			System.out.println("Please select a valid value");
-			selectLetter(player1Letter);
+			selectLetter(ticTacToeGame);
 		}
-		return player1Letter;
 	}
 
 	/**
-	 * @param player1Turn
-	 * @param player1Letter
-	 * @param board
+	 * This method is deciding the move to be done
+	 * 
+	 * @param ticTacToeGame
+	 * @return
 	 */
-	private static boolean makeAMove(boolean player1Turn, String player1Letter, char[] board) {
-		String player2Letter = player1Letter.equalsIgnoreCase("X") ? "0" : "X";
-		Scanner sc = new Scanner(System.in);
-		if (player1Turn) {
-			System.out.println("Player1's turn(" + player1Letter + ")");
+	private boolean makeAMove(TicTacToeGame ticTacToeGame) {
+		String player2Letter = ticTacToeGame.player1Letter.equalsIgnoreCase("X") ? "0" : "X";
+		if (ticTacToeGame.player1Turn) {
+			System.out.println("Player1's turn(" + ticTacToeGame.player1Letter + ")");
 		} else {
 			System.out.println("Player2's turn(" + player2Letter + ")");
+			checkForWinningMove(ticTacToeGame);
 		}
-
-		int a = sc.nextInt();
-		if (a >= 1 && a <= 9) {
-			System.out.println(board[a]);
-			if (board[a] != 'X' && board[a] != '0') {
-				if (player1Turn) {
-					board[a] = player1Letter.toCharArray()[0];
+		int index = scanner.nextInt();
+		if (index >= 1 && index <= 9) {
+			if (ticTacToeGame.board[index] != 'X' && ticTacToeGame.board[index] != '0') {
+				if (ticTacToeGame.player1Turn) {
+					ticTacToeGame.board[index] = ticTacToeGame.player1Letter.toCharArray()[0];
 				} else {
-					board[a] = player2Letter.toCharArray()[0];
+					ticTacToeGame.board[index] = player2Letter.toCharArray()[0];
 				}
-				player1Turn = !player1Turn;
-				drawBoard(board);
-				playerHasWon(board);
-				String output = "";
-				if (playerHasWon(board) == 'X') {
-					output = player1Letter.equalsIgnoreCase("X") ? "Player1 won" : "Player2 won";
-					System.out.println(output);
-					return true;
-				} else if (playerHasWon(board) == '0') {
-					output = player1Letter.equalsIgnoreCase("0") ? "Player1 won" : "Player2 won";
-					System.out.println(output);
-					return true;
+				ticTacToeGame.player1Turn = !ticTacToeGame.player1Turn;
+				createBoard(ticTacToeGame);
+				boolean gameStatus = checkGameStatus(ticTacToeGame);
+				if (gameStatus) {
+					System.out.println(ticTacToeGame.gameStatusMessage);
+					System.exit(0);
 				} else {
-					if (!isTie(board)) {
-						makeAMove(player1Turn, player1Letter, board);
-					} else {
-						System.out.println("Game ended in a tie");
-						return true;
-					}
+					makeAMove(ticTacToeGame);
 				}
-
 			} else {
 				System.out.println("This index is already taken. Please choose another index");
-				makeAMove(player1Turn, player1Letter, board);
+				makeAMove(ticTacToeGame);
 			}
 		} else {
 			System.out.println("Please enter a valid input");
-			makeAMove(player1Turn, player1Letter, board);
+			makeAMove(ticTacToeGame);
 		}
 		return false;
 	}
 
 	/**
-	 * @param board
+	 * This method is suggesting the player to decide which move to move to win the
+	 * game
+	 * 
+	 * @param ticTacToeGame
+	 */
+	private void checkForWinningMove(TicTacToeGame ticTacToeGame) {
+		char[] board = ticTacToeGame.board;
+		char player2Letter = ticTacToeGame.player1Letter.equalsIgnoreCase("X") ? '0' : 'X';
+
+		if ((board[2] == player2Letter && board[3] == player2Letter)
+				|| (board[4] == player2Letter && board[7] == player2Letter)
+				|| (board[5] == player2Letter && board[9] == player2Letter)) {
+			if (isIndexFree(1, board)) {
+				System.out.println("You have winning chance at index 1");
+			}
+		}
+
+		if ((board[1] == player2Letter && board[3] == player2Letter)
+				|| (board[5] == player2Letter && board[8] == player2Letter)) {
+			if (isIndexFree(2, board)) {
+				System.out.println("You have winning chance at index 2");
+			}
+		}
+
+		if ((board[1] == player2Letter && board[2] == player2Letter)
+				|| (board[6] == player2Letter && board[9] == player2Letter)
+				|| (board[5] == player2Letter && board[7] == player2Letter)) {
+			if (isIndexFree(3, board)) {
+				System.out.println("You have winning chance at index 3");
+			}
+		}
+
+		if ((board[5] == player2Letter && board[6] == player2Letter)
+				|| (board[1] == player2Letter && board[7] == player2Letter)) {
+			if (isIndexFree(4, board)) {
+				System.out.println("You have winning chance at index 4");
+			}
+		}
+		if ((board[4] == player2Letter && board[6] == player2Letter)
+				|| (board[2] == player2Letter && board[8] == player2Letter)
+				|| (board[9] == player2Letter && board[1] == player2Letter)) {
+			if (isIndexFree(5, board)) {
+				System.out.println("You have winning chance at index 5");
+			}
+		}
+		if ((board[4] == player2Letter && board[5] == player2Letter)
+				|| (board[3] == player2Letter && board[9] == player2Letter)) {
+			if (isIndexFree(6, board)) {
+				System.out.println("You have winning chance at index 6");
+			}
+		}
+		if ((board[1] == player2Letter && board[4] == player2Letter)
+				|| (board[8] == player2Letter && board[9] == player2Letter)
+				|| (board[3] == player2Letter && board[5] == player2Letter)) {
+			if (isIndexFree(7, board)) {
+				System.out.println("You have winning chance at index 7");
+			}
+		}
+		if ((board[7] == player2Letter && board[9] == player2Letter)
+				|| (board[2] == player2Letter && board[5] == player2Letter)) {
+			if (isIndexFree(8, board)) {
+				System.out.println("You have winning chance at index 8");
+			}
+		}
+		if ((board[7] == player2Letter && board[8] == player2Letter)
+				|| (board[3] == player2Letter && board[6] == player2Letter)
+				|| (board[1] == player2Letter && board[5] == player2Letter)) {
+			if (isIndexFree(9, board)) {
+				System.out.println("You have winning chance at index 9");
+			}
+		}
+	}
+
+	/**
+	 * This method is checking the current status of the game
+	 * 
+	 * @param ticTacToeGame
 	 * @return
 	 */
-	private static boolean isTie(char[] board) {
+	private boolean checkGameStatus(TicTacToeGame ticTacToeGame) {
+		boolean status = false;
+		if (playerHasWon(ticTacToeGame.board) == 'X') {
+			ticTacToeGame.gameStatusMessage = ticTacToeGame.player1Letter.equalsIgnoreCase("X") ? "Player1 won"
+					: "Player2 won";
+			status = true;
+		} else if (playerHasWon(ticTacToeGame.board) == '0') {
+			ticTacToeGame.gameStatusMessage = ticTacToeGame.player1Letter.equalsIgnoreCase("0") ? "Player1 won"
+					: "Player2 won";
+			status = true;
+		} else {
+			if (!isTie(ticTacToeGame.board)) {
+				makeAMove(ticTacToeGame);
+			} else {
+				ticTacToeGame.gameStatusMessage = "Game ended in a tie";
+				status = true;
+			}
+		}
+		return status;
+
+	}
+
+	/**
+	 * This method is checking if there is tie between the players
+	 */
+	private boolean isTie(char[] board) {
 		for (int j = 1; j <= 9; j++) {
 			if (board[j] != 'X' && board[j] != '0') {
 				return false;
@@ -122,33 +210,46 @@ public class TicTacToeGame {
 	}
 
 	/**
-	 * @param player1Turn
+	 * This method is checking if there are any free space in the board
+	 * 
+	 * @param index
+	 * @param board
 	 * @return
 	 */
-	private static boolean toss(boolean player1Turn) {
+	private boolean isIndexFree(int index, char[] board) {
+		if (board[index] == '\u0000') {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * This method is used to toss to decide who plays first
+	 * 
+	 * @param ticTacToeGame
+	 */
+	private void toss(TicTacToeGame ticTacToeGame) {
 		System.out.println("Player1 to call the toss: Head or Tail");
-		Scanner sc = new Scanner(System.in);
-		String headOrTail = sc.nextLine();
+		String headOrTail = scanner.nextLine();
 		if (headOrTail.equalsIgnoreCase("head") || headOrTail.equalsIgnoreCase("tail")) {
-			System.out.println("....");
-			System.out.println("....");
 			double random = Math.random();
 			if (headOrTail.equalsIgnoreCase("head") && random >= 0.5) {
-				player1Turn = true;
+				ticTacToeGame.player1Turn = true;
 				System.out.println("Player1 won the toss");
 			} else {
-				player1Turn = false;
+				ticTacToeGame.player1Turn = false;
 				System.out.println("Player2 won the toss");
 			}
 		} else {
 			System.out.println("Incorrect input. Please call Head or Tail");
-			toss(player1Turn);
+			toss(ticTacToeGame);
 		}
-
-		return player1Turn;
 	}
 
-	private static char playerHasWon(char[] board) {
+	/**
+	 * This method is written to check which player has won
+	 */
+	private char playerHasWon(char[] board) {
 		for (int a = 1; a < 9; a++) {
 			String line = null;
 
